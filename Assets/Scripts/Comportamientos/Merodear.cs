53921a6406e59f6296eb097f815f430ccd5bd4af
Nahia <82345386+nahigles@users.noreply.github.com>
2025-05-23 13:29:55 +0200

    Merodeo mediofuncional

--- Assets/Scripts/Comportamientos/Merodear.cs
+++ Assets/Scripts/Comportamientos/Merodear.cs
@@ -35,32 +35,84 @@ namespace UCM.IAV.Movimiento
         float t = 3.0f;
         float actualT = 2.0f;
 
+        // Dir vertical
+        int dirY = 1;
+        bool up = true;
+
         ComportamientoDireccion lastDir = new ComportamientoDireccion();
 
-        public override ComportamientoDireccion GetComportamientoDireccion(){
+        public override ComportamientoDireccion GetComportamientoDireccion()
+        {
 
 
-            if (t >= actualT) 
+            if (t >= actualT)
             {
+
+                Debug.Log("Vel Angular: " + lastDir.angular);
+                Debug.Log("Vel Lineal: " + lastDir.lineal);
+
                 float rotation = Random.Range(-1f, 1f) * agente.aceleracionAngularMax;
 
                 lastDir.angular += rotation;
 
                 lastDir.lineal = OriToVec(lastDir.angular).normalized; //la transformamos en su velocidad y la normalizamos
-                lastDir.lineal *= agente.aceleracionMax;  //va a maxima velocidad
+
+                Vector3 auxDir = Vector3.zero;
+                if (up)                
+                    auxDir = new Vector3(lastDir.lineal.x, -lastDir.lineal.y + dirY, lastDir.lineal.z);
+                else
+                    auxDir = new Vector3(lastDir.lineal.x, -lastDir.lineal.y -2*dirY, lastDir.lineal.z);
+                
+
+                lastDir.lineal = auxDir;
+
+
+                //lastDir.lineal *= agente.aceleracionMax;  //va a maxima velocidad
 
                 //calculamos el tiempo q vamos a estar en esta dirección
                 actualT = Random.Range(tiempoMinimo, tiempoMaximo);
                 t = 0;
+                up = !up;
             }
             else
             {
                 t += Time.deltaTime;
             }
 
+            lastDir.angular = 0;
+
+            if(transform.position.y < 0.5)
+            {
+                lastDir.lineal = new Vector3(lastDir.lineal.x, -lastDir.lineal.y + dirY, lastDir.lineal.z);
+            }
+            else if(transform.position.y > 4.5)
+            {
+                lastDir.lineal = new Vector3(lastDir.lineal.x, -lastDir.lineal.y - dirY, lastDir.lineal.z);
+            }
+            else if(transform.position.z > 5)
+            {
+                lastDir.lineal = new Vector3(lastDir.lineal.x, lastDir.lineal.y, -lastDir.lineal.z - dirY*5);
+            }
+            else if(transform.position.z < -11)
+            {
+                lastDir.lineal = new Vector3(lastDir.lineal.x, lastDir.lineal.y, -lastDir.lineal.z + dirY*5);
+            }
+            else if(transform.position.x < -19)
+            {
+                lastDir.lineal = new Vector3(-lastDir.lineal.x + dirY * 5, lastDir.lineal.y, lastDir.lineal.z);
+            }
+            else if(transform.position.x > 17)
+            {
+                lastDir.lineal = new Vector3(-lastDir.lineal.x - dirY * 5, lastDir.lineal.y, lastDir.lineal.z);
+            }
+
             return lastDir;
         }
 
+        private void Start()
+        {
+            //lastDir.lineal = 
+        }
     }
 }
 
